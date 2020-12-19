f124b6e362023755d0b487c74a0afb5e1bf5bd69
ZamiRich <argashokovzamir@gmail.com>
2020-12-19 15:58:22 +0100

    Project created. No code yet.

---
+++ ThirdLesson/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ThirdLesson
+//
+//  Created by ZamiRich on 19.12.2020.
+//  Copyright © 2020 ZamiRich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
